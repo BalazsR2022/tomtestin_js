72b45787bcfccf5482635836051295bc14d5812f
Balázs Réka <balazs.reka@ktch.hu>
2023-05-09 20:10:19 +0200

    Kezdés

---
+++ src/app.js
@@ -0,0 +1,35 @@
+const doc = {
+    weightInput: document.querySelector('#weight'),
+    heightInput: document.querySelector('#height'),
+    indexInput: document.querySelector('#index'),
+    calcButton: document.querySelector('#calcButton')
+};
+
+const state = {
+    index : 0
+};
+
+window.addEventListener('load', () => {
+    init();
+});
+
+function init (){
+    doc.calcButton.addEventListener('click', () =>{
+        console.log('működik')
+        startCalc();
+    });
+}
+
+function startCalc(){
+    let weight = doc.weightInput.value;
+    let height = doc.heightInput.value;
+    state.index = calcBodyindex(weight,height);
+    doc.indexInput.value = state.index;
+
+}
+
+function calcBodyindex(weight, height){
+    return weight / Math.pow(height, 2);
+}
+
+
